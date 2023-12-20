b340c6a28280fa0d716080e94bb6ebed87b9dc40
Ivax <68695142+Ivanopulos@users.noreply.github.com>
2023-12-20 13:45:58 +0300

    Update DeltMkd.py

--- DeltMkd.py
+++ DeltMkd.py
@@ -26,35 +26,44 @@ step1 = "разово"  # формирование df["Голосов"]
 step2 = "проверка на базу"  # формирование df["Голосов"]
 # Загрузка данных
 dvhod = pd.read_csv('data.csv', sep=";")
-matched_dvhod = pd.DataFrame()  # заготовка куда оставляем подтвержденные
-dvhod['alname'] = dvhod['last_name']+" "+dvhod['first_name']+" "+dvhod['middle_name']  # ФИО
 df = pd.read_excel('combined_data_with_votes.xlsx')
+
+# Обработка ФИО
+dvhod['alname'] = dvhod['last_name'] + " " + dvhod['first_name'] + " " + dvhod['middle_name']
+dvhod['alname'] = dvhod['alname'].str.lower()  # Приведение к нижнему регистру
+
+# Создание столбца для сравнения ФИО в df
+df['ФИО правообладателя'] = df['ФИО правообладателя'].str.lower()
+
+matched_dvhod = pd.DataFrame()  # Для подтвержденных записей
+
 for _, row in dvhod.iterrows():
     variable = row['property_ownership']
-    print(variable)
-    # Шаг 1: Поиск по полному соответствию
+    alname = row['alname']
+
+    # Шаг 1: Поиск по полному соответствию номера права
     matches = df[df["Номер права"] == variable]
-    print(matches)
+
     # Шаг 2: Поиск по регулярному выражению, если на шаге 1 ничего не найдено
     if matches.empty:
         pattern = re.compile(r'[^\d]')
         variable_re = pattern.sub('', variable)
         df['Номер права без букв'] = df["Номер права"].str.replace(pattern, '', regex=True)
         matches = df[df['Номер права без букв'] == variable_re]
-    # Шаг 3: Поиск по ФИО правообладателя, если найдено больше одного соответствия
-    if len(matches) > 1:
-        best_match = process.extractOne(variable, matches['ФИО правообладателя'])[2]
-        if best_match < len(matches):
-            matches = matches.iloc[[best_match]]
-    # Если найдено соответствие, вписываем 1 в столбец "найдено"
+
+    # Шаг 3: Поиск по ФИО правообладателя
+    if not matches.empty:
+        matches = matches[matches['ФИО правообладателя'] == alname]
+
+    # Шаг 4: Обработка найденных совпадений
     if not matches.empty:
         df.loc[matches.index, 'Login'] = row['user_login']
         matched_dvhod = pd.concat([matched_dvhod, pd.DataFrame([row.to_dict()])], ignore_index=True)
     else:
         with open('не_найденные.txt', 'a') as file:
             file.write(variable + '\n')
+
+# Сохранение результатов
 df.to_excel("combined_data_with_votes.xlsx", index=False, engine="openpyxl")
-# Сохраняем результат
 matched_dvhod.drop('alname', axis=1, inplace=True)
-matched_dvhod.to_csv('data-exit.csv', encoding='utf-8', index=False, sep=";")  # на выход
-df.to_excel("combined_data_with_votes.xlsx", index=False, engine="openpyxl")
+matched_dvhod.to_csv('data-exit.csv', encoding='utf-8', index=False, sep=";")
